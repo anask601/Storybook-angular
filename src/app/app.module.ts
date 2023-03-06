a8241ed0753efb6036e5c3a4a16031bfdff24761
anask601 <anask60155@gmail.com>
2023-03-06 09:02:38 +0530

    ✨ added Storybook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import ButtonComponent from 'src/stories/button.component';
+import HeaderComponent from 'src/stories/header.component';
+import PageComponent from 'src/stories/page.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ButtonComponent, HeaderComponent, PageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
